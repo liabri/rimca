97e49983dfe97c2da37489e128591d4e0549c088
lb06 <lbcat@tutanota.com>
2022-02-19 11:26:10 +0100

    launch error

--- src/librimca/src/vanilla/mod.rs
+++ src/librimca/src/vanilla/mod.rs
@@ -2,7 +2,8 @@ mod api;
 
 use crate::Instance;
 use crate::download::DownloadSequence;
-use crate::error::Error;
+use crate::launch::LaunchSequence;
+use crate::error::{ Error, LaunchError };
 
 pub struct Vanilla {
 	version: String,
@@ -31,4 +32,12 @@ impl DownloadSequence for Instance<Vanilla> {
 	fn spawn_thread(&self) -> Result<(), Error> {
 		Ok(())
 	}
+}
+
+impl LaunchSequence for Instance<Vanilla> {
+	fn get_main_class(&self) -> Result<&str, LaunchError> { todo!() }
+	fn execute(&self, jvm_args: Vec<String>, main_class: &str, game_opts: Vec<String>) -> Result<(), LaunchError> { todo!() }
+	fn get_jvm_arguments(&self, classpath: &str) -> Result<Vec<String>, LaunchError> { todo!() }
+	fn get_game_options(&self) -> Result<Vec<String>, LaunchError> { todo!() }
+	fn get_classpath(&self) -> Result<String, LaunchError> { todo!() }
 }
