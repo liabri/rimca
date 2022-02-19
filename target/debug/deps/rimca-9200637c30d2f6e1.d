34f2e981b11416bd8eeedf87180b4dff948676b8
lb06 <lbcat@tutanota.com>
2022-02-19 10:39:02 +0100

    remove target

--- target/debug/deps/rimca-9200637c30d2f6e1.d
+++
@@ -1,5 +0,0 @@
-/home/liabri/dev/rimca/target/debug/deps/rimca-9200637c30d2f6e1.rmeta: src/rimca/src/main.rs
-
-/home/liabri/dev/rimca/target/debug/deps/rimca-9200637c30d2f6e1.d: src/rimca/src/main.rs
-
-src/rimca/src/main.rs:
